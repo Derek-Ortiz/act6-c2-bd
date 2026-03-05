4ed712e7929d02414dcb2c1e7a1507649fae1d32
Derek-Ortiz <243742@ids.upchiapas.edu.mx>
2026-03-05 17:09:41 -0600

    Creacion de la BD

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM postgres:15-alpine
+
+
+COPY schema.sql /docker-entrypoint-initdb.d/
---
+++ db/schema.sql
@@ -0,0 +1,36 @@
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(150) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL, 
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
+
+
+CREATE TABLE IF NOT EXISTS movies (
+    id SERIAL PRIMARY KEY,
+    external_id VARCHAR(50) UNIQUE NOT NULL, 
+    title VARCHAR(255) NOT NULL,
+    description TEXT,
+    release_year INTEGER,
+    cover_image VARCHAR(255),
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
+
+
+CREATE TABLE IF NOT EXISTS favorites (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    movie_id INTEGER NOT NULL REFERENCES movies(id) ON DELETE CASCADE,
+    is_active BOOLEAN DEFAULT TRUE, -- TRUE = Favorito activo, FALSE = Removido (Soft Delete)
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    
+    
+    CONSTRAINT unique_user_movie UNIQUE (user_id, movie_id) 
+);
+
+
+INSERT INTO users (name, email, password) 
+VALUES ('Admin', 'admin@proyecto.com', 'hash_de_prueba_123')
+ON CONFLICT (email) DO NOTHING;
